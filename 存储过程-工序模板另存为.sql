a768679a078cfc7d2ed2ddfc03975af77335c0bb
AeroYoung <saucadcam@126.com>
2016-10-03 09:14:24 +0800

    放假也在努力工作

---
+++ 存储过程-工序模板另存为.sql
@@ -0,0 +1,35 @@
+-- =============================================
+-- Author:		杨尧
+-- Create date: 2016-10-01
+-- Description:	工序模板另存为新模板
+-- =============================================
+if OBJECT_ID(N'dbo.ProcessTempSaveAsNewTemp', N'P') is NOT null 
+DROP procedure  ProcessTempSaveAsNewTemp 
+GO
+CREATE PROCEDURE ProcessTempSaveAsNewTemp
+	@newTempName varchar(MAX) = '',
+	@oldTempName varchar(MAX) = ''	
+AS
+BEGIN
+    insert into MES_ProcessTempList_table(tempName,tempCode,[readOnly]) 
+    select @newTempName as tempName,tempCode,[readOnly] from MES_ProcessTempList_table where tempName=@oldTempName
+
+	insert into MES_ProcessTempContent_table([tempName]
+      ,[processID]
+      ,[processName]
+      ,[processContent]
+      ,[preTime]
+      ,[singleTime]
+      ,[outSourcing]
+      ,[machine]
+      ,[remark]) 
+    select @newTempName as tempName,[processID]
+      ,[processName]
+      ,[processContent]
+      ,[preTime]
+      ,[singleTime]
+      ,[outSourcing]
+      ,[machine]
+      ,[remark] from MES_ProcessTempContent_table where tempName=@oldTempName
+END
+GO
