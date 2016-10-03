033e5951155ce25c1a6b3bc7be309fc2a4465676
AeroYoung <saucadcam@126.com>
2016-10-03 08:33:35 +0800

    放假了还这么早起

--- 存储过程-向PBOM插入工序模板.sql
+++ 存储过程-向PBOM插入工序模板.sql
@@ -13,9 +13,9 @@ BEGIN
   INSERT INTO MES_ProcessBOM_table
   (BOM_ID,processName,processID,processContent,preTime,singleTime,outSourcing,availMachine)    
   select ID,processName,processID,processContent,preTime,singleTime,outSourcing,machine
-  from BOM_EXCEL_table,MES_ProcessTempContent_table,MES_ProcessTempStandard_table
-  where BOM_EXCEL_Table.code=MES_ProcessTempStandard_table.code 
-  AND MES_ProcessTempContent_table.tempName=MES_ProcessTempStandard_table.tempName
+  from BOM_EXCEL_table,MES_ProcessTempContent_table,Map_ProcessTempStandard_table
+  where BOM_EXCEL_Table.code=Map_ProcessTempStandard_table.code 
+  AND MES_ProcessTempContent_table.tempName=Map_ProcessTempStandard_table.tempName
   AND (manufacture_type='二次加工' or manufacture_type='非标')
   AND system_code=@systemCode
 END
