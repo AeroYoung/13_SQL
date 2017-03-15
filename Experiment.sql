4528ea4216bc00fa9d2c864fb0eaab2a1287375b
AeroYoung <saucadcam@126.com>
2017-03-15 08:09:55 +0800

    sql

--- Experiment.sql
+++ Experiment.sql
@@ -6,66 +6,54 @@
 if OBJECT_ID(N'dbo.VehicleCostStatistic', N'P') is NOT null 
 DROP procedure  VehicleCostStatistic 
 GO
-
-if OBJECT_ID(N'dbo.CanisterCostStatistic', N'P') is NOT null 
-DROP procedure  CanisterCostStatistic 
-
-GO
 CREATE PROCEDURE VehicleCostStatistic
 	@beginDate varchar(MAX) = '2017/01/01',--开始日期
 	@endDate varchar(MAX) = '2018/01/01',--结束日期
 	@merge bit = 1, -- 是否合并N月公告
 	@groupString VARCHAR(MAX) = 'SHED,taskType',--GROUP BY
-	@queryString VARCHAR(MAX) = ' where billCheck=''是'''--查询条件
+	@yGroup VARCHAR(MAX) = '',
+	@queryString VARCHAR(MAX) = ' where billCheck=''否'''--查询条件
 AS
 BEGIN
---1 复制数据
+--1 复制数据到#data
 
-	select cast(year(preStartDate) as varchar)+'年' as y,cast(month(preStartDate) as varchar)+'月' as m,* 
-		into #data from VehicleDataTable WHERE preStartDate BETWEEN @beginDate AND @endDate
-	
-	if (@merge=1) --把N月公告合并
-	begin
-		update #data set taskType='公告' where taskType like '公告'
-	end
+select cast(year(preStartDate) as varchar)+'年' as y,cast(month(preStartDate) as varchar)+'月' as m,
+	SHED,taskType,testType,manufacturer,billCheck,[standard],cost
+	into #data from VehicleDataTable WHERE preStartDate BETWEEN @beginDate AND @endDate
 
-	--2 查询
-	declare @strSql VARCHAR(MAX)
+if (@merge=1) --把N月公告合并
+begin
+	update #data set taskType='公告' where taskType like '公告'
+end
 
+select * from #data --tables[0]
+
+--2 查询条件
+DECLARE @strSql VARCHAR(MAX)
+
+if(@yGroup<>'')
+BEGIN
+
+	SET @strSql = 'select distinct '+@yGroup+' from #data' --tables[1] -series
+	EXEC(@strSql)
+
+	--tables[2] 在C#中使用WHERE得到分组值
+	SET @groupString += ',' + @yGroup
 	SET @strSql = 'SELECT sum(cast(cost as float)) AS COST,count(*) AS NUM,'+@groupString+' FROM #data '
-	set @strSql += @queryString
-	set @strSql += ' GROUP BY '+@groupString
+	SET @strSql += ' GROUP BY '+@groupString
 
 	EXEC(@strSql)
 
-END
-
-
-CREATE PROCEDURE CanisterCostStatistic
-	@beginDate varchar(MAX) = '2017/06/01',--开始日期
-	@endDate varchar(MAX) = '2018/01/01',--结束日期
-	@merge bit = 1, -- 是否合并N月公告
-	@groupString VARCHAR(MAX) = 'SHED,taskType',--GROUP BY
-	@queryString VARCHAR(MAX) = ' where billCheck=''是'''--查询条件
-AS
+END --if(@yGroup<>'')
+ELSE
 BEGIN
---1 复制数据
 
-	select cast(year(receiveDate) as varchar)+'年' as y,cast(month(receiveDate) as varchar)+'月' as m,* 
-		into #data from CanisterDataTable WHERE receiveDate BETWEEN @beginDate AND @endDate
-	
-	if (@merge=1) --把N月公告合并
-	begin
-		update #data set taskType='公告' where taskType like '公告'
-	end
-
-	--2 查询
-	declare @strSql VARCHAR(MAX)
-
-	SET @strSql = 'SELECT sum(cast(cost as float)) AS COST,count(*) AS NUM,'+@groupString+' FROM #data '
-	set @strSql += @queryString
-	set @strSql += ' GROUP BY '+@groupString
+	SELECT TOP 1 '全部' AS EXPR1 FROM #data --tables[1] -series
+	--tables[2] 在C#中使用WHERE得到分组值
+	SET @strSql = 'SELECT sum(cast(cost as float)) AS COST,count(*) AS NUM,'+@groupString+',''全部'' as yGroup FROM #data '
+	SET @strSql += ' GROUP BY '+@groupString
 
 	EXEC(@strSql)
+END --if(@yGroup<>'') ELSE
 
-END
+END --PROCEDURE
